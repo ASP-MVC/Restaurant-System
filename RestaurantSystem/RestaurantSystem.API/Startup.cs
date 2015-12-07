111e8e5687515c97c4f316059e8c16d80e09b3e9
Kristian Lyubenov <kristian.lubenov@gmail.com>
2015-12-07 17:20:36 +0200

    Init API

---
+++ RestaurantSystem/RestaurantSystem.API/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(RestaurantSystem.API.Startup))]
+
+namespace RestaurantSystem.API
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
