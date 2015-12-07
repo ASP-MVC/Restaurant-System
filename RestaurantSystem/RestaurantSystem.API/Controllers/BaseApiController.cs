c8e2900ae25b143235e2e0c855a9ac3955de9444
Kristian Lyubenov <kristian.lubenov@gmail.com>
2015-12-07 17:59:07 +0200

    Added BaseApiController.cs

---
+++ RestaurantSystem/RestaurantSystem.API/Controllers/BaseApiController.cs
@@ -0,0 +1,31 @@
+﻿using RestaurantSystem.Data.UoW;
+using RestaurantSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace RestaurantSystem.API.Controllers
+{
+    public abstract class BaseApiController : ApiController
+    {
+        public BaseApiController(IRestaurantData data)
+        {
+            this.Data = data;
+        }
+
+        protected IRestaurantData Data { get; private set; }
+
+        protected bool IsAuthenticated()
+        {
+            if (this.User == null)
+            {
+                return false;
+            }
+            return this.User.Identity.IsAuthenticated;
+        }
+    }
+}
