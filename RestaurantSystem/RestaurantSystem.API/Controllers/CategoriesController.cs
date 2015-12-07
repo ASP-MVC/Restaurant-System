0c8dad7a0612fa94566e9074c2fdf96c26ae4ed5
Kristian Lyubenov <kristian.lubenov@gmail.com>
2015-12-07 22:33:29 +0200

    Restaurants Controller added

---
+++ RestaurantSystem/RestaurantSystem.API/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿namespace RestaurantSystem.API.Controllers
+{
+    using System.Linq;
+    using System.Web.Http;
+
+    using RestaurantSystem.Data.UoW;
+
+    public class CategoriesController : BaseApiController
+    {
+        public CategoriesController(IRestaurantData data)
+            : base(data)
+        {
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            var categories =
+                this.Data.Categories.All().OrderBy(x => x.Id).ToList();
+
+            return this.Ok(categories);
+        }
+    }
+}
